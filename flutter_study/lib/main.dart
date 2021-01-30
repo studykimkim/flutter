2fd83d35eb5ccfba14fe7eef727b745bee611131
Jae Yeon <jaeyeon423@gmail.com>
2021-01-30 15:45:35 +0900

    micard complete

--- flutter_study/lib/main.dart
+++ flutter_study/lib/main.dart
@@ -11,74 +11,69 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50,
-              backgroundColor: Colors.red,
-              backgroundImage: AssetImage('images/sample1.jpg'),
-            ),
-            Text(
-              'JaeYeon kim',
-              style: TextStyle(
-                  fontSize: 40, color: Colors.white, fontFamily: 'Pacifico'),
-            ),
-            Text(
-              'this is sample app',
-              style: TextStyle(
-                  color: Colors.teal.shade100,
-                  fontSize: 20,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold),
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    '+82 010 6375 9204',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/sample1.jpg'),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
+              Text(
+                'JaeYeon kim',
+                style: TextStyle(
+                    fontSize: 40, color: Colors.white, fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'this is sample app',
+                style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontSize: 20,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+                width: 140.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '010-6375-9204',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  )),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: ListTile(
+                  leading: Icon(
                     Icons.email,
                     color: Colors.teal,
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
+                  title: Text(
                     'jaeyeon423@gmail.com',
                     style: TextStyle(
+                      fontSize: 20.0,
                       color: Colors.teal.shade900,
-                      fontSize: 20,
                     ),
                   ),
-                ],
+                ),
               ),
-            ),
-          ],
-        )),
+            ],
+          ),
+        ),
       ),
     );
   }
