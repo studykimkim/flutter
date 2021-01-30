1b7432e253a428fbf5a5208679d6f86d5a23919a
Jae Yeon <jaeyeon423@gmail.com>
2021-01-31 00:10:13 +0900

    dice - random function

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -22,6 +23,7 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +36,7 @@ class _DicePageState extends State<DicePage> {
               onPressed: () {
                 print("button1 is pressed");
                 setState(() {
-                  leftDiceNumber = 2;
+                  leftDiceNumber = Random().nextInt(6) + 1;
                 });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
@@ -44,6 +46,9 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 print("button2 is pressed");
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
               },
               child: Image.asset('images/dice1.png'),
             ),
