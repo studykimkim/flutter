f2015fcc0020d9f3c6d9e4eab10dbddf8ca6205b
jaeyeon <jaeyeon423@gmail.com>
2021-02-03 22:37:18 +0900

    xylophone : complete

--- xylophone2/lib/main.dart
+++ xylophone2/lib/main.dart
@@ -10,58 +10,34 @@ class XylophoneApp extends StatelessWidget {
     final player = AudioCache();
     player.play('note$soundNumber.wav');
   }
+
+  Expanded buildKey({Color color , int soundNumber}){
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                FlatButton(
-                  color: Colors.red,
-                  onPressed: () {
-                    playSound(1);
-                  },
-                ),
-                FlatButton(
-                  color: Colors.orange,
-                  onPressed: () {
-                    playSound(2);
-                  },
-                ),
-                FlatButton(
-                  color: Colors.yellow,
-                  onPressed: () {
-                    playSound(3);
-                  },
-                ),
-                FlatButton(
-                  color: Colors.green,
-                  onPressed: () {
-                    playSound(4);
-                  },
-                ),
-                FlatButton(
-                  color: Colors.teal,
-                  onPressed: () {
-                    playSound(5);
-                  },
-                ),
-                FlatButton(
-                  color: Colors.blue,
-                  onPressed: () {
-                    playSound(6);
-                  },
-                ),
-                FlatButton(
-                  color: Colors.purple,
-                  onPressed: () {
-                    playSound(7);
-                  },
-                ),
-              ],
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color : Colors.red, soundNumber :1),
+              buildKey(color : Colors.orange, soundNumber :2),
+              buildKey(color : Colors.yellow, soundNumber :3),
+              buildKey(color : Colors.green, soundNumber :4),
+              buildKey(color : Colors.blue, soundNumber :5),
+              buildKey(color : Colors.teal, soundNumber :6),
+              buildKey(color : Colors.purple, soundNumber :7),
+            ],
           ),
         ),
       ),
