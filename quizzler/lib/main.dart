e019b29b8d18b69e81593ebcad3f9201ebfcc48c
jaeyeon <jaeyeon423@gmail.com>
2021-02-04 20:02:28 +0900

    Quizzler : start

---
+++ quizzler/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizePage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizePage extends StatefulWidget {
+  @override
+  _QuizePageState createState() => _QuizePageState();
+}
+
+class _QuizePageState extends State<QuizePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('hello'),
+    );
+  }
+}
