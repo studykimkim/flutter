2f6afc8eabab945607cdb134f7e62c927cd006db
jaeyeon <jaeyeon423@gmail.com>
2021-02-13 15:30:18 +0900

    try&catch throw ??

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -7,7 +7,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -15,13 +14,32 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
+    Position position;
+    try {
+      position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+    } on Exception catch (e) {
+      print(e);
+    }
     print(position);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    String myMargin = 'abc';
+    double myMarginAsADouble;
+
+    try {
+      myMarginAsADouble = double.parse(myMargin);
+    } catch (e) {
+      print(e);
+      // myMarginAsADouble = 30.0;
+    }
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(myMarginAsADouble ?? 10),
+        color: Colors.teal,
+      ),
+    );
   }
 }
